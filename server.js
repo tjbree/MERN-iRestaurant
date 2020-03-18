48f2aaeb2253136c90665aba048b527db0c7c792
Yanyan Zhao <breezhao.au@gmail.com>
2020-03-18 15:14:27 +1100

    deploy fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/auth', authRouter)
 // Serve static resource if it is in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'))
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
